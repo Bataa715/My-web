aed33a8cb5f0d53b0f49594d4425c6008e1a4850
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-14 03:46:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7407955062-e8810",
+  "appId": "1:70574616620:web:9615bd1a10f9c09ee62189",
+  "apiKey": "AIzaSyDx1EpImAU5zjcv4Sgox6cNa1GNzwk6ffc",
+  "authDomain": "studio-7407955062-e8810.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "70574616620"
+};
