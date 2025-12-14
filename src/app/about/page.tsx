0d1fb170f7d23836c93682c3bb8238824a94ca2b
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-14 06:16:39 +0000

    about huudas deer appiin udsen headeriig nem

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,12 +4,10 @@ import Skills from '@/components/sections/skills';
 
 export default function AboutPage() {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <main className="flex-1">
-        <Hero />
-        <Projects />
-        <Skills />
-      </main>
-    </div>
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+    </>
   );
 }
