e7642933c102c80c7dc0c5a6b1e154f4a0e6297b
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-17 08:54:24 +0000

    huudasiig buuten ashigla dooroo zai uldeed baina . eswel center bolgo (_

--- src/app/tools/pomodoro/page.tsx
+++ src/app/tools/pomodoro/page.tsx
@@ -7,11 +7,11 @@ import Timer from "@/app/tools/pomodoro/components/Timer";
 export default function PomodoroPage() {
 
     return (
-        <div className="space-y-8">
-            <BackButton />
-            <div className="flex justify-center pt-8">
-                <Timer />
+        <div className="relative flex flex-col items-center justify-center min-h-[calc(100vh-200px)]">
+            <div className="absolute top-0 left-0">
+                <BackButton />
             </div>
+            <Timer />
         </div>
     );
 }
