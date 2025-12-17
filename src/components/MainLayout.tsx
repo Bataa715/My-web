b15089db8f5311fb5e10cf840d10a8652f34c1c6
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-17 07:20:42 +0000

    zugeerl hureend l uilchile shuudee zasraagui baina

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -15,7 +15,7 @@ export default function MainLayout({
   const showHeaderFooter = !noHeaderFooterPaths.includes(pathname);
 
   return (
-    <div className="relative flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col bg-background">
       {showHeaderFooter && <Header />}
       <main className="flex-1">
          <div className="p-4 sm:p-6 lg:p-8">
