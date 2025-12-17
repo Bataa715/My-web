ee417078061c5dde5c0d75d862cb9b2fe2bd41eb
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-17 08:21:43 +0000

    .bbh-bartle-regular {   font-family: "BBH Bartle", sans-serif;   font-we

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -129,7 +129,7 @@ const Header = () => {
                           href={link.href}
                           onClick={() => setIsOpen(false)}
                           className={cn(
-                            "text-sm font-medium transition-colors hover:text-primary",
+                            "font-bartle text-lg transition-colors hover:text-primary",
                             (pathname.startsWith(link.href) && link.href !== '/') || pathname === link.href ? "text-primary font-semibold" : "text-muted-foreground"
                           )}
                         >
@@ -148,7 +148,7 @@ const Header = () => {
                       key={link.href}
                       href={link.href}
                       className={cn(
-                        "transition-colors hover:text-primary",
+                        "font-bartle text-lg transition-colors hover:text-primary",
                         (pathname.startsWith(link.href) && link.href !== '/') || pathname === link.href ? "text-primary font-semibold" : "text-muted-foreground"
                       )}
                     >
