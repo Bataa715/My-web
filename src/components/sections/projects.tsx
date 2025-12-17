e2b6c1a761a6d8c86f408d70fd3ba259f060c485
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-17 08:46:19 +0000

    animatsionii tsagaanii hemjeeg ni 0.2 bolgo (_for element div_)

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -92,7 +92,7 @@ export default function Projects() {
                     initial={{ opacity: 0, scale: 0.8 }}
                     animate={{ opacity: 1, scale: 1 }}
                     exit={{ opacity: 0, scale: 0.8 }}
-                    transition={{ duration: 0.3, delay: index * 0.05 }}
+                    transition={{ duration: 0.2, delay: index * 0.05 }}
                     onMouseMove={handleMouseMove}
                     className="education-card rounded-lg"
                   >
