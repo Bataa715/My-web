1c7bb49ce9f0b3d4c6cb639fd7075713ed9cc3e3
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-17 06:57:26 +0000

    delete (_for element p_)

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -59,9 +59,6 @@ export default function Projects() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-headline font-bold tracking-tighter sm:text-5xl">Миний төслүүд</h2>
-            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Миний хийсэн ажлууд, технологийн шийдлүүдтэй танилцана уу.
-            </p>
           </div>
         </div>
 
