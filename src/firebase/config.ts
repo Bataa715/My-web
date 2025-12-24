cf44a76fceafd4e963ae784d7cf8f0c3eed5c2c1
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-24 14:18:09 +0000

    export const firebaseConfig = {   apiKey: "AIzaSyDx1EpImAU5zjcv4Sgox6cNa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -21,7 +21,7 @@ if (missingEnvVars.length > 0 && typeof window !== 'undefined') {
 }
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDx1EpImAU5zjcv4Sgox6cNa1GNzwk6ffc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
