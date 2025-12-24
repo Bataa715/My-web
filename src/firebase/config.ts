060287c761b847f094800d981da8791aa6947ea1
Batmyagmar B. <bbatmyagmar7@gmail.com>
2025-12-24 12:47:20 +0000

    Failed to load resource: the server responded with a status of 503 ()Und

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDx1EpImAU5zjcv4Sgox6cNa1GNzwk6ffc",
-  authDomain: "studio-7407955062-e8810.firebaseapp.com",
-  projectId: "studio-7407955062-e8810",
-  storageBucket: "studio-7407955062-e8810.firebasestorage.app",
-  messagingSenderId: "70574616620",
-  appId: "1:70574616620:web:30a4d2d37e35b520e62189"
+  apiKey: "AIzaSyDsw6ox_fmME37xw9qQhmv6MJW53CD7O68",
+  authDomain: "my-app2-91085973-754a0.firebaseapp.com",
+  projectId: "my-app2-91085973-754a0",
+  storageBucket: "my-app2-91085973-754a0.appspot.com",
+  messagingSenderId: "560945514682",
+  appId: "1:560945514682:web:7f61c3b169f53835e07e15",
+  measurementId: "G-J6CM1J53W4"
 };
