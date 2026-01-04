a2f2bd30a967f6697ff6823674e3cda313a2fdbb
Batmyagmar B. <bbatmyagmar7@gmail.com>
2026-01-04 12:01:58 +0000

    ondor 178  torson odoor 2004 05 25 gesen 2 kartiig nem

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -126,11 +126,11 @@ export default function AboutPage() {
               setPersonalInfo(data.personalInfo);
             } else {
               const defaultInfo: PersonalInfoType[] = [
-                  { value: "21", label: "Нас", icon: 'Cake' },
-                  { value: "6-р сарын 16", label: "Төрсөн өдөр", icon: 'CalendarDays' },
+                  { value: "20", label: "Нас", icon: 'Cake' },
+                  { value: "5-р сарын 25", label: "Төрсөн өдөр", icon: 'CalendarDays' },
                   { value: "INTJ", label: "MBTI", icon: 'User' },
                   { value: "Ихэр", label: "Орд", icon: 'Gemini' },
-                  { value: "175cm", label: "Өндөр", icon: 'Scaling' },
+                  { value: "178cm", label: "Өндөр", icon: 'Scaling' },
               ];
               await updateDoc(userDocRef, { personalInfo: defaultInfo });
               setPersonalInfo(defaultInfo);
